aeb4985c39b3b54eec96e38d607f276406f22faa
Mohit <mohit.kumar5037@gmail.com>
2020-04-02 09:00:24 +0530

    Initial Commit

---
+++ SnapChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnapChat
+//
+//  Created by Mohit on 02/04/20.
+//  Copyright © 2020 Practise. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
